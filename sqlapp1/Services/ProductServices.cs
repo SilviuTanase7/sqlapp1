ace8731a651989e7faab580c784b3c9fe850fe5e
silviu.tanase <silviu.tanase@LST0006>
2022-06-16 12:56:37 +0300

    Add project files.

---
+++ sqlapp1/Services/ProductServices.cs
@@ -0,0 +1,54 @@
+﻿using sqlapp1.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp1.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "webappsofteh.database.windows.net";
+        private static string db_user = "sqladmin";
+        private static string db_password = "zx12NM90";
+        private static string db_database = "webdbsofteh";
+
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            List<Product> _product_lst = new List<Product>();
+
+            string statement = "SELECT ProductID,ProductName,Quantity FROM Products";
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(statement, conn);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ProductID = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(product);
+                }
+                conn.Close();
+                return _product_lst;
+            }
+        }
+    }
+}
